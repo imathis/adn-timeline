3b30e4829f33765b015a3c65a1d6df6f03188ca9
Brandon Mathis <brandon@imathis.com>
2013-03-02 11:53:41 -0600

    cleaned up gh-pages branch

--- package.rb
+++
@@ -1,8 +0,0 @@
-require 'coffee-script'
-require 'uglifier'
-
-js = CoffeeScript.compile File.read("adn-timeline.coffee")
-File.open("javascripts/adn-timeline.js", 'w') { |f| f.write js }
-
-ugjs = Uglifier.new.compile js
-File.open("javascripts/adn-timeline.min.js", 'w') { |f| f.write ugjs }
